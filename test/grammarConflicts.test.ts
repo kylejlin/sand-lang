34132bbddd0b8fcf2cb65f4760a846646a263ccd
Kyle Lin <kylejlin.us@gmail.com>
2019-12-13 18:04:36 -0800

    Count each type of grammar conflict in regression tests

--- test/grammarConflicts.test.ts
+++ test/grammarConflicts.test.ts
@@ -18,17 +18,37 @@ test("sand.jison does not have new conflicts", () => {
 
   expect(out).toMatchSnapshot();
 
-  const numberOfConflicts = getNumberOfConflicts(out);
+  const conflictCount = getConflictCount(out);
 
-  expect(numberOfConflicts).toMatchSnapshot();
+  expect(conflictCount).toMatchSnapshot();
 });
 
-function getNumberOfConflicts(logs: string): number {
-  const matches = logs.match(/Conflict in grammar/g);
+function getConflictCount(logs: string): ConflictCount {
+  const matches = logs.match(
+    /Conflict in grammar[\s\S]*?(?=(?:Conflict in grammar|States with conflicts|$))/g,
+  );
 
   if (matches === null) {
-    return 0;
+    return { total: 0, reduceShift: 0, reduceReduce: 0 };
   } else {
-    return matches.length;
+    return {
+      total: matches.length,
+      reduceShift: matches.filter(isReduceShift).length,
+      reduceReduce: matches.filter(isReduceReduce).length,
+    };
   }
+}
+
+function isReduceShift(match: string): boolean {
+  return /- reduce[\s\S]*- shift/.test(match);
+}
+
+function isReduceReduce(match: string): boolean {
+  return /- reduce[\s\S]*- reduce/.test(match);
+}
+
+interface ConflictCount {
+  total: number;
+  reduceShift: number;
+  reduceReduce: number;
 }
