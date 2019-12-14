6bd2c2b332907808234e0ca2ed19b3aa9770d3df
Kyle Lin <kylejlin.us@gmail.com>
2019-12-14 14:50:40 -0800

    Extract addApi()

---
+++ src/parser/addApi.ts
@@ -0,0 +1,54 @@
+import {
+  BinaryExpr,
+  BinaryOperation,
+  camelCase,
+  ConstraintType,
+  IfAlternativeType,
+  JisonNodeLocation,
+  merge,
+  NodeType,
+  UnaryExpr,
+  UnaryOperation,
+} from "../ast";
+import { wrapPrimitiveIfNeeded } from "../sandTypes";
+import { Parser } from "./parser.generated";
+
+export default function addApi(parser: Parser) {
+  const { yy } = parser.parser;
+
+  yy.NodeType = NodeType;
+  yy.IfAlternativeType = IfAlternativeType;
+  yy.ConstraintType = ConstraintType;
+
+  yy.binaryExpr = function binaryExpr(
+    operation: BinaryOperation,
+    left: any,
+    right: any,
+    location: JisonNodeLocation,
+  ): BinaryExpr {
+    return {
+      type: NodeType.BinaryExpr,
+      operation,
+      left,
+      right,
+      location: camelCase(location),
+    };
+  };
+
+  yy.unaryExpr = function unaryExpr(
+    operation: UnaryOperation,
+    right: any,
+    location: JisonNodeLocation,
+  ): UnaryExpr {
+    return {
+      type: NodeType.UnaryExpr,
+      operation,
+      right,
+      location: camelCase(location),
+    };
+  };
+
+  yy.wrapPrimitiveIfNeeded = wrapPrimitiveIfNeeded;
+  yy.camelCase = camelCase;
+  yy.merge = merge;
+}
--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,54 +1,6 @@
-import {
-  BinaryExpr,
-  BinaryOperation,
-  camelCase,
-  ConstraintType,
-  IfAlternativeType,
-  NodeType,
-  UnaryExpr,
-  UnaryOperation,
-  merge,
-  JisonNodeLocation,
-} from "../ast";
-import { wrapPrimitiveIfNeeded } from "../sandTypes";
+import addApi from "./addApi";
 import parser from "./parser.generated";
 
-const { yy } = parser.parser;
-
-yy.NodeType = NodeType;
-yy.IfAlternativeType = IfAlternativeType;
-yy.ConstraintType = ConstraintType;
-
-yy.binaryExpr = function binaryExpr(
-  operation: BinaryOperation,
-  left: any,
-  right: any,
-  location: JisonNodeLocation,
-): BinaryExpr {
-  return {
-    type: NodeType.BinaryExpr,
-    operation,
-    left,
-    right,
-    location: camelCase(location),
-  };
-};
-
-yy.unaryExpr = function unaryExpr(
-  operation: UnaryOperation,
-  right: any,
-  location: JisonNodeLocation,
-): UnaryExpr {
-  return {
-    type: NodeType.UnaryExpr,
-    operation,
-    right,
-    location: camelCase(location),
-  };
-};
-
-yy.wrapPrimitiveIfNeeded = wrapPrimitiveIfNeeded;
-yy.camelCase = camelCase;
-yy.merge = merge;
+addApi(parser);
 
 export default parser;
